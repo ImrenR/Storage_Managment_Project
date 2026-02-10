e787d9e63d930b80daf951fb672fa27caf870b3f
ImrenR <imrenrahbay@gmail.com>
2026-02-10 16:55:57 +0000

    Methods

--- src/Methods.java
+++ src/Methods.java
@@ -3,8 +3,11 @@ import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Methods implements GirisCikisInterface {
-
-
+    public static final String W = "\u001B[37m";
+    public static final String R = "\u001B[31m";
+    public static final String G = "\u001B[32m";
+    public static final String Y = "\u001B[33m";
+    public static final String B = "\u001B[34m";
     static Scanner input= new Scanner(System.in);
     static int id=1000;
     static HashMap<Integer,Products> urunlerMap=new HashMap<>(); // urunler obkesini store edecek bos map
