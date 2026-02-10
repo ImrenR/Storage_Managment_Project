aa5dc1186fce33d6395230b70888c9e9e99236c0
ImrenR <imrenrahbay@gmail.com>
2026-02-10 17:08:27 +0000

    Methods

--- src/Methods.java
+++ src/Methods.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Methods implements GirisCikisInterface {
@@ -25,10 +26,50 @@ public class Methods implements GirisCikisInterface {
                 + "   ____________________              ____________________   \n"
                 + "   | 5-URUN CIKISI    |              |  6-BITIRME       |   \n"
                 + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  " + R);
+
+
+        System.out.println("Yapmak istediginiz islemi seciniz");
+       try {
+
+
+           int secim = input.nextInt();
+
+           switch (secim) {
+               case 1:
+                   urunTanimlama();
+                   break;
+               case 2:
+                   System.out.println();
+                   break;
+               case 3:
+                   System.out.println();
+                   break;
+               case 4:
+                   System.out.println();
+                   break;
+               case 5:
+                   System.out.println();
+                   break;
+               case 6:
+                   System.out.println();
+                   break;
+
+               default:
+                   System.out.println("hatali giris yaptiniz tekrar deneyin");
+                   break;
+           }
+       }catch (InputMismatchException e){
+           System.out.println("Sayi disinda bir karakter girilemez");
+           input.nextLine(); // dumy : hayalet komut.. Aabir bos komut aciyorum ki yeni komuta girebilsin pattern erro vermesin
+           girisPaneli();
+       }
+    }
+
+    private void urunTanimlama() {
     }
 
     @Override
     public void cikisYap() {
-
+        System.out.println("App ten cikisiniz yapildi");
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,6 @@
 public class Runner  extends Methods{
     static void main() {
-
+Runner depoCalis = new Runner();
+depoCalis.girisPaneli();
     }
 }
